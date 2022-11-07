d7386a0a6c3b444c3b18917c9367e64f0c515b09
Leno <bartlomiej.sikora@op.pl>
2022-11-07 14:01:40 +0100

    Create table via hibernate

--- car-parts/src/main/java/com/javamypackage/orm/OrmAppApplication.java
+++ car-parts/src/main/java/com/javamypackage/orm/OrmAppApplication.java
@@ -1,13 +1,18 @@
 package com.javamypackage.orm;
 
+import com.javamypackage.orm.car.entity.Car;
+import com.javamypackage.orm.category.entity.Category;
+import com.javamypackage.orm.invoice.entity.Invoice;
+import com.javamypackage.orm.invoiceitem.entity.InvoiceItem;
+import com.javamypackage.orm.part.entity.Part;
+import com.javamypackage.orm.user.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrmAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrmAppApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(OrmAppApplication.class, args);}
 
 }
